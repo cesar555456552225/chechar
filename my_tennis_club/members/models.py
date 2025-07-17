60445daa2f14b3619348d651804a7af3f611027f
herrerace497@gmail.com <Cesar Manosalva>
2025-07-17 09:08:55 -0500

    Se trabaja con la base de datos

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  phone = models.CharField(null=True)
+  joined_date = models.DateField(null=True)
+  gender = models.DateField(max_length=500, null = True)
+  birthay = models.DateField(null=True)
+  email = models.EmailField(max_length=255, null=True)
