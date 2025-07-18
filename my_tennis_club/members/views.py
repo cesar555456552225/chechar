c057aa064e6eca6d611dc8fa121b48e849fb7779
herrerace497@gmail.com <Cesar Manosalva>
2025-07-18 11:58:05 -0500

    se hizo una nueva plantilla y se actualizo la base de datos

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -29,3 +29,10 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def testing (request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits':['Manzana', 'Banano', 'Cereza'],
+  }
+  return HttpResponse(template.render(context, request))
